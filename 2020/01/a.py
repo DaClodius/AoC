05918187e7d6ae3c7fb662ff7a3aaf85ffcdf3e7
Robert Clodius <robert.clodius@lht.dlh.de>
2020-12-03 08:42:49 +0100

    improve two sum problem

--- 2020/01/a.py
+++ 2020/01/a.py
@@ -1,10 +1,8 @@
 with open('d') as f:
     n = [int(l.rstrip()) for l in f]
 
-def s():
-    for i, n1 in enumerate(n):
-        for n2 in n[i:]:
-            if (n1 + n2 == 2020):
-                return n1 * n2
-
-print(s())
+s = set()
+for i in n:
+    s.add(i)
+    if (2020 - i in s):
+        print(i * (2020 - i))
