4709bd1b3d41da623edb6152bd6aec7c79888831
Robert Clodius <robert.clodius@lht.dlh.de>
2020-12-03 08:41:57 +0100

    remove redundant import

--- 2020/02/b.py
+++ 2020/02/b.py
@@ -1,10 +1,9 @@
-from operator import xor
 import re
 
 r = 0
 for l in open('d'):
     a = re.split('-| |: ', l.rstrip())
-    if xor(a[3][int(a[0])-1] == a[2], a[3][int(a[1])-1] == a[2]):
+    if (a[3][int(a[0])-1] == a[2]) ^ (a[3][int(a[1])-1] == a[2]):
         r += 1
 
 print(r)
