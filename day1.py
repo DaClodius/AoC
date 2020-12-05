32e52c68c4b600e00a2bfd694268c8fa0f01f3d9
Robert Clodius <robert.clodius@lht.dlh.de>
2020-12-05 12:08:33 +0100

    refactor: remove redundant parameter passing

--- day1.py
+++ day1.py
@@ -1,17 +1,16 @@
-def part_one(nums, set_nums):
+def part_one(nums):
     for num in nums:
-        if 2020 - num in set_nums:
+        if 2020 - num in set(nums):
             return num * (2020 - num)
 
-def part_two(nums, set_nums):
-    for num in nums:
+def part_two(nums):
+    for num1 in nums:
         for num2 in nums:
-            if 2020 - num - num2 in set_nums:
-                return num * num2 * (2020 - num - num2)
+            if 2020 - num1 - num2 in set(nums):
+                return num1 * num2 * (2020 - num1 - num2)
 
 with open('input/day1') as _file:
     nums = [int(line.rstrip()) for line in _file]
-    set_nums = set(nums)
 
-print(part_one(nums, set_nums))
-print(part_two(nums, set_nums))
+print(part_one(nums))
+print(part_two(nums))
