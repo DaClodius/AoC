d9ad4882a27c1b511b4aaf7084f0388b3faf41fb
Robert Clodius <robert.clodius@lht.dlh.de>
2020-12-14 11:59:32 +0100

    feat: 14th day

---
+++ day14.py
@@ -0,0 +1,27 @@
+def part_one(value):
+    memory[0][index] = int(''.join([bitmask[pointer] if bitmask[pointer].isnumeric() else value[pointer] for pointer in range(len(bitmask))]), 2)
+
+def part_two(index):
+    index = [bitmask[pointer] if bitmask[pointer] == '1' else index[pointer] for pointer in range(len(index))]
+    for numeric in range(pow(2, bitmask.count('X'))):
+        for binary_index, binary_digit in enumerate(binary_string(numeric, bitmask.count('X'))):
+            index[len(bitmask) - len(bitmask.split('X', binary_index + 1)[-1]) - 1] = binary_digit
+            memory[1][int(''.join(index), 2)] = value
+
+def binary_string(string, length):
+    binary = '{0:b}'.format(string)
+    return '0' * (length - len(binary)) + binary
+
+with open('input/day14') as file:
+    memory = [{}, {}]
+    for line in [line.strip() for line in file]:
+        if line.startswith('mask'):
+            bitmask = line[7:]
+        else:
+            index = int(line[4:line.index(']')])
+            value = int(line[line.index('=') + 1:])
+            part_one(list(binary_string(value, len(bitmask))))
+            part_two(list(binary_string(index, len(bitmask))))
+            
+print(sum(memory[0].values()))
+print(sum(memory[1].values()))
