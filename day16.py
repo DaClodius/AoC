c44b10302eaa7609f91967a70a4dfba3deb96883
Robert Clodius <robert.clodius@lht.dlh.de>
2020-12-16 17:27:21 +0100

    refactor: cast int beforehand

--- day16.py
+++ day16.py
@@ -12,7 +12,7 @@ def part_one() -> int:
     for ticket in nearby_tickets:
         for field in ticket:
             if not field_applies_to_any_rule(field):
-                sum_of_invalid_fields += int(field)
+                sum_of_invalid_fields += field
     return sum_of_invalid_fields
 
 def part_two() -> int:
@@ -33,7 +33,7 @@ def part_two() -> int:
                 sub_item[1].remove(order_item[1][0])
 
     for value in dict(filter(lambda e: e[0][:9] == 'departure', field_order.items())).values():
-        product *= int(your_ticket[value[0]])
+        product *= your_ticket[value[0]]
     return product
 
 def get_valid_tickets() -> List[Ticket]:
@@ -43,15 +43,15 @@ def get_valid_tickets() -> List[Ticket]:
             valid_tickets.append(ticket)
     return valid_tickets
 
-def field_applies_to_any_rule(field: str) -> bool:
+def field_applies_to_any_rule(field: int) -> bool:
     for rule in rules:
         if field_applies_to_rule(field, rule):
             return True
     return False
 
-def field_applies_to_rule(field: str, rule: any) -> bool:
+def field_applies_to_rule(field: int, rule: any) -> bool:
     for range_ in rule[1].split(' or '):
-        if int(range_.split('-')[0]) <= int(field) <= int(range_.split('-')[1]):
+        if int(range_.split('-')[0]) <= field <= int(range_.split('-')[1]):
             return True
     return False
 
@@ -59,9 +59,9 @@ with open('input/day16') as file:
     data_parts = file.read().split('\n\n')
     for line in data_parts[0].strip().split('\n'):
         rules.append((line[:line.index(':')], line[line.index(':') + 2:]))
-    your_ticket = data_parts[1].strip().split('\n')[1].split(',')
+    your_ticket = [int(n) for n in data_parts[1].strip().split('\n')[1].split(',')]
     for line in data_parts[2].strip().split('\n')[1:]:
-        nearby_tickets.append(line.split(','))
+        nearby_tickets.append([int(n) for n in line.split(',')])
 
 print(part_one())
 print(part_two())
